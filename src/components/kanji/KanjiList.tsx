10d6a7c24d280ec264ef4fd3d44319854709f25d
Mfikryyyyyy11 <mfikryhaikal8@gmail.com>
2025-08-07 07:11:53 +0700

    some changes

--- src/components/kanji/KanjiList.tsx
+++ src/components/kanji/KanjiList.tsx
@@ -48,22 +48,25 @@ export default function KanjiList() {
     <div className="z-50">
       <div className="mb-12 flex items-center justify-center gap-4 max-md:gap-2">
         {levels.map((lvl) => (
-          <button key={lvl} className={`px-4 py-2 rounded-md border-2 border-yellow-400 cursor-pointer max-md:text-sm  ${level === lvl ? 'bg-red-500 text-yellow-400 font-bold' : 'bg-white text-gray-700'}`} onClick={() => setLevel(lvl)}>
+          <button key={lvl} className={`px-4 py-2 rounded-md border-2 border-yellow-400 cursor-pointer max-md:text-sm ${level === lvl ? 'bg-red-500 text-yellow-400 font-bold' : 'bg-white text-gray-700'}`} onClick={() => setLevel(lvl)}>
             {lvl}
           </button>
         ))}
       </div>
 
-      <div className="grid grid-cols-6 gap-4 max-md:grid-cols-3 max-lg:grid-cols-4 ">
-        {currentKanjis.length > 0 ? currentKanjis.map((kanji, index) => <KanjiCard key={index} kanji={kanji} />) : <p className="col-span-6 flex justify-center items-center ">Data untuk level {level} belum tersedia.</p>}
+      <div className="grid grid-cols-6 gap-4 max-md:grid-cols-3 max-lg:grid-cols-4">
+        {currentKanjis.length > 0 ? currentKanjis.map((kanji, index) => <KanjiCard key={index} kanji={kanji} />) : <p className="col-span-6 flex justify-center items-center">Data untuk level {level} belum tersedia.</p>}
       </div>
 
       {totalPages > 1 && (
-        <div className="flex justify-center mt-10 gap-4">
-          <div className="flex gap-2 items-center">
+        <div className="flex justify-center mt-10 overflow-x-auto px-2">
+          <div className="flex gap-2 max-md:gap-1 items-center">
             {getPageNumbers()[0] > 1 && (
               <>
-                <button onClick={() => setCurrentPage(1)} className={`w-12 h-12 rounded-md text-sm cursor-pointer border-2 border-yellow-400 ${currentPage === 1 ? 'bg-dark text-yellow-400 font-bold' : 'bg-white text-black'}`}>
+                <button
+                  onClick={() => setCurrentPage(1)}
+                  className={`w-12 h-12 max-md:w-8 max-md:h-8 rounded-md text-sm max-md:text-xs cursor-pointer border-2 border-yellow-400 ${currentPage === 1 ? 'bg-dark text-yellow-400 font-bold' : 'bg-white text-black'}`}
+                >
                   1
                 </button>
                 {getPageNumbers()[0] > 2 && <span className="px-2">...</span>}
@@ -74,7 +77,7 @@ export default function KanjiList() {
               <button
                 key={page}
                 onClick={() => setCurrentPage(page)}
-                className={`w-12 h-12 rounded-md text-sm cursor-pointer border-2 border-yellow-400 ${currentPage === page ? 'bg-red-500 text-yellow-400 font-bold' : 'bg-white text-black'}`}
+                className={`w-12 h-12 max-md:w-8 max-md:h-8 rounded-md text-sm max-md:text-xs cursor-pointer border-2 border-yellow-400 ${currentPage === page ? 'bg-red-500 text-yellow-400 font-bold' : 'bg-white text-black'}`}
               >
                 {page}
               </button>
@@ -85,7 +88,7 @@ export default function KanjiList() {
                 {getPageNumbers().slice(-1)[0] < totalPages - 1 && <span className="px-2">...</span>}
                 <button
                   onClick={() => setCurrentPage(totalPages)}
-                  className={`w-12 h-12 rounded-md text-sm cursor-pointer border-2 border-yellow-400 ${currentPage === totalPages ? 'bg-dark text-yellow-400 font-bold' : 'bg-white text-black'}`}
+                  className={`w-12 h-12 max-md:w-8 max-md:h-8 rounded-md text-sm max-md:text-xs cursor-pointer border-2 border-yellow-400 ${currentPage === totalPages ? 'bg-dark text-yellow-400 font-bold' : 'bg-white text-black'}`}
                 >
                   {totalPages}
                 </button>
